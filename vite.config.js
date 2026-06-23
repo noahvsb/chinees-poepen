b3632e3e4d39c292917a225192af6ed8197073da
noahvsb <noahvansteenbrugge@gmail.com>
2026-06-23 12:05:00 +0200

    update config to build to /docs instead of /dist

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,10 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/chinees-poepen/',
+  build: {
+    outDir: 'docs',
+  },
   plugins: [
     vue(),
     vueDevTools(),
